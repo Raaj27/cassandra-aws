1b74860c5161a83862d65de6e30af0ee5cfed5bc
cfernandes <cfernandes@loyalty.com>
2017-08-29 10:09:43 -0400

    more comments allow feeding of the cluster name via command line arguments

--- truststore-setup.sh
+++ truststore-setup.sh
@@ -8,12 +8,12 @@ KEY_STORE="$KEY_STORE_PATH/cassandra.keystore"
 PKS_KEY_STORE="$KEY_STORE_PATH/cassandra.pks12.keystore"
 TRUST_STORE="$KEY_STORE_PATH/cassandra.truststore"
 PASSWORD=$1
-CLUSTER_NAME=test
+CLUSTER_NAME=$2
 CLUSTER_PUBLIC_CERT="$KEY_STORE_PATH/CLUSTER_${CLUSTER_NAME}_PUBLIC.cer"
 CLIENT_PUBLIC_CERT="$KEY_STORE_PATH/CLIENT_${CLUSTER_NAME}_PUBLIC.cer"
 
 ### Cluster key setup.
-# Create the cluster key for cluster communication.
+# Create the cluster keystore for cluster communication.
 keytool -genkey -keyalg RSA -alias "${CLUSTER_NAME}_CLUSTER" -keystore "$KEY_STORE" -storepass "$PASSWORD" -keypass "$PASSWORD" \
 -dname "CN=LoyaltyOne Image $CLUSTER_NAME cluster, OU=LoyaltyOne, O=LoyaltyOne, L=Toronto, ST=ON, C=CA, DC=LoyaltyOne, DC=com" \
 -validity 36500
@@ -22,7 +22,7 @@ keytool -genkey -keyalg RSA -alias "${CLUSTER_NAME}_CLUSTER" -keystore "$KEY_STO
 keytool -export -alias "${CLUSTER_NAME}_CLUSTER" -file "$CLUSTER_PUBLIC_CERT" -keystore "$KEY_STORE" \
 -storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
 
-# Import the identity of the cluster public cluster key into the trust store so that nodes can identify each other.
+# Import the identity of the cluster'a public key into the trust store so that nodes can identify each other.
 keytool -import -v -trustcacerts -alias "${CLUSTER_NAME}_CLUSTER" -file "$CLUSTER_PUBLIC_CERT" -keystore "$TRUST_STORE" \
 -storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
 
@@ -37,14 +37,17 @@ keytool -genkey -keyalg RSA -alias "${CLUSTER_NAME}_CLIENT" -keystore "$KEY_STOR
 keytool -export -alias "${CLUSTER_NAME}_CLIENT" -file "$CLIENT_PUBLIC_CERT" -keystore "$KEY_STORE" \
 -storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
 
-# Import the identity of the client pub  key into the trust store so nodes can identify this client.
+# Import the identity of the client public key into the trust store so nodes can identify this client.
 keytool -importcert -v -trustcacerts -alias "${CLUSTER_NAME}_CLIENT" -file "$CLIENT_PUBLIC_CERT" -keystore "$TRUST_STORE" \
 -storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
 
 keytool -importkeystore -srckeystore "$KEY_STORE" -destkeystore "$PKS_KEY_STORE" -deststoretype PKCS12 \
 -srcstorepass "$PASSWORD" -deststorepass "$PASSWORD"
 
+# Client's public key
 openssl pkcs12 -in "$PKS_KEY_STORE" -nokeys -out "${CLUSTER_NAME}_CLIENT.cer.pem" -passin "pass:$PASSWORD"
+
+# Client's private key
 openssl pkcs12 -in "$PKS_KEY_STORE" -nodes -nocerts -out "${CLUSTER_NAME}_CLIENT.key.pem" -passin "pass:$PASSWORD"
 
 sed -i.bak -e "s/keystore_password: cassandra/keystore_password: $PASSWORD/g" cassandra-config/cassandra.yaml
